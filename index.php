bdcbcc73c0c1a1f8691c5fda7e84ed88a162ad44
jayakrishna8686 <jayakrishna8686@gmail.com>
2024-12-10 21:39:09 +0530

    created block theme again

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-function enqueue_vjkwebsolutions_scripts() {
-    wp_enqueue_style('vjkwebsolutions-style', get_stylesheet_uri());
-}
-add_action('wp_enqueue_scripts', 'enqueue_vjkwebsolutions_scripts');
-?>
