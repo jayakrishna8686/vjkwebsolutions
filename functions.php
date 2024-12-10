9195a3950eb550113ce7d5ebfdfc2d6e7f7981dd
jayakrishna8686 <jayakrishna8686@gmail.com>
2024-12-10 17:48:22 +0530

    Added Editor Support

--- functions.php
+++ functions.php
@@ -1,5 +1,24 @@
 <?php
-function vjkwebsolutions_theme_support(){
+if (!defined('ABSPATH')) exit;
+
+function vjkwebsolutions_theme_setup() {
+    // Add theme support for various features
     add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_theme_support('responsive-embeds');
+    add_theme_support('editor-styles');
+    add_theme_support('html5', array(
+        'search-form',
+        'comment-form',
+        'comment-list',
+        'gallery',
+        'caption',
+        'style',
+        'script'
+    ));
+    
+    // Essential for block editor
+    add_theme_support('wp-block-styles');
+    add_theme_support('align-wide');
 }
-add_action('after_setup_theme', 'vjkwebsolutions_theme_support');
+add_action('after_setup_theme', 'vjkwebsolutions_theme_setup');
